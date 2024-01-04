e691d6475e1c22ff80569bd4dbb1f9b3cfcaac8d
xixeonxim <bbiyakeke@duksung.ac.kr>
2024-01-04 20:57:06 +0900

    [feat]bartermain 페이지 업데이트

--- front/src/pages/Barter/Main/index.jsx
+++ front/src/pages/Barter/Main/index.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Nav from '../../../components/Nav';
+import Footer from '../../../components/Footer';
+import classes from './Main.module.css';
+import rabbit2Image from "../../../assets/rabbit2.png";
 
 function BarterMain() {
     return (   
     <div>
         <Nav />
-        물물교환 메인페이지
+        <div className={classes.main1Bg}>
+        <div className={classes.rabbit2}>
+        <Link to="/bartermain"><img src={rabbit2Image} alt="귀여운 토끼" /></Link>
+        </div>
+        <div className={classes.mainText1}>
+          <p className={classes.surHeading}>물물교환</p>
+          <div className={classes.mainHeading}>이웃간 물물교환</div>
+          <p className={classes.subHeading}>동네 주민들과 가깝고 따뜻한 거래를<br />지금 경험해보세요.</p>
+        </div>
+      </div>
+       <Footer />
     </div>
     );
 }
